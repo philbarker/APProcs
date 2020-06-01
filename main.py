afa5faf5d2c1dcf2a4649b91eeb1d00527d0a941
Phil Barker <phil.barker@pjjk.co.uk>
2020-06-01 18:09:13 +0100

    better arguments

--- main.py
+++ main.py
@@ -8,11 +8,24 @@ def get_args():
         description="Read an application profile from simple csv file and output some RDFS for the profile.",
     )
     parser.add_argument(
-        "-if",
-        "--infile",
+        "infile",
         type=str,
         default="input/ap_bookclub.csv",
         help="input file name of Application Profile csv",
+    )
+    parser.add_argument(
+        "-d",
+        "--dump",
+        type=str,
+        default="False",
+        help="Dump (print) the AP once loaded",
+    )
+    parser.add_argument(
+        "-y",
+        "--yama",
+        type=str,
+        default="False",
+        help="Convert and dump (print) the AP as YAMA",
     )
     return parser.parse_args()
 
@@ -20,9 +33,11 @@ def get_args():
 if __name__ == "__main__":
     args = get_args()
     ap = APProcs(args.infile)
-    ap.dump()
-    yama = ap.build_yama()
-    ap.dump_yama(yama)
+    if args.dump.lower() in ["true", "t", "1", "y", "yes"]:
+        ap.dump()
+    if args.yama.lower() in ["true", "t", "1", "y", "yes"]:
+        yama = ap.build_yama()
+        ap.dump_yama(yama)
 #    base = ap.make_base_graph()
 #    ap_graph = ap.make_ap_graph(base)
 #    print(ap_graph.serialize(format="turtle").decode("utf-8"))
